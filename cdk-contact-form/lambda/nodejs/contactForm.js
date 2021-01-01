951b0f965fb9b237c6e5be97f720471703ba5660
Jose Maria Hidalgo Garcia <jhidalgo3@gmail.com>
2021-01-01 18:58:29 +0100

    Add lambda

---
+++ cdk-contact-form/lambda/nodejs/contactForm.js
@@ -0,0 +1,10 @@
+#! ./lambda/nodejs/contactForm.js (G4)
+
+exports.handler = async function (event) {
+    console.log("request:", JSON.stringify(event, undefined, 2));
+    return {
+        statusCode: 200,
+        headers: { "Content-Type": "text/plain" },
+        body: `Hello there\n`
+    };
+};
